e4c07dc360d5824a4a58605df7844d788328f403
sitaram <sitaram@LAPTOP-CDVHSVHO>
2019-06-10 00:05:26 +0530

    wITH RUNNERS

---
+++ src/main/java/com/app/SpringBootWithRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithRunnerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+@Component
+@Order(10)
+public class AppRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("from runner-b");
+
+	}
+
+}
